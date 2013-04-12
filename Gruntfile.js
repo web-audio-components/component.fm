d36c2b10772f133fc6389406555878121f829a84
Nick Thompson <ncthom91@gmail.com>
2013-04-11 20:51:59 -0400

    Gruntfile for compiling stylus

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    stylus: {
+      compile: {
+        options: {
+          'include css': true
+        },
+        files: {
+          'public/build/site.css': 'public/assets/styles/main.styl'
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+};

1497b4f6bd67dc5991af756b2b06fcfd547d12a4
Nick Thompson <ncthom91@gmail.com>
2013-04-11 21:14:02 -0400

    Trying to browserify and shim jquery in grunt. Not working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 
+var browserify = require('browserify')
+  , shim = require('browserify-shim');
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
@@ -26,5 +29,23 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('browserify', function () {
+    console.log('hi');
+    shim(browserify(), {
+      jquery: {
+        path: './public/assets/scripts/vendor/jquery-1.9.1.min.js',
+        exports: '$'
+      }
+    })
+    .require(require.resolve('./public/assets/scripts/vendor/lodash-1.1.1.min.js'), { expose: 'underscore' })
+    .require(require.resolve('./public/assets/scripts/vendor/backbone-1.0.0.min.js'), { expose: 'backbone' })
+    .require(require.resolve('./public/app/main.js'), { entry: true })
+    .bundle(function (err, source) {
+      console.log('bye');
+      if (err) return console.error(err);
+      fs.writeFileSync('./public/build/site.js', source);
+    });
+  });
 
 };
